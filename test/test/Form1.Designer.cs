5463af4c7cf9f9fbf86b7506c82d1264b5b16e0d
Alena-Samus <elensawelu@gmail.com>
2022-02-10 14:17:00 +0300

    preliminarily

---
+++ test/test/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿
+namespace test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.previousBtn = new System.Windows.Forms.Button();
+            this.nextBtn = new System.Windows.Forms.Button();
+            this.stopBtn = new System.Windows.Forms.Button();
+            this.QTBox = new System.Windows.Forms.TextBox();
+            this.Answ1rB = new System.Windows.Forms.RadioButton();
+            this.Answ2rB = new System.Windows.Forms.RadioButton();
+            this.Answ3rB = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.NameTb = new System.Windows.Forms.TextBox();
+            this.OkBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // previousBtn
+            // 
+            this.previousBtn.Location = new System.Drawing.Point(74, 384);
+            this.previousBtn.Name = "previousBtn";
+            this.previousBtn.Size = new System.Drawing.Size(95, 23);
+            this.previousBtn.TabIndex = 3;
+            this.previousBtn.Text = "Предыдущий";
+            this.previousBtn.UseVisualStyleBackColor = true;
+            this.previousBtn.Click += new System.EventHandler(this.previousBtn_Click);
+            // 
+            // nextBtn
+            // 
+            this.nextBtn.Location = new System.Drawing.Point(187, 384);
+            this.nextBtn.Name = "nextBtn";
+            this.nextBtn.Size = new System.Drawing.Size(86, 23);
+            this.nextBtn.TabIndex = 4;
+            this.nextBtn.Text = "Следующий";
+            this.nextBtn.UseVisualStyleBackColor = true;
+            this.nextBtn.Click += new System.EventHandler(this.nextBtn_Click);
+            // 
+            // stopBtn
+            // 
+            this.stopBtn.Location = new System.Drawing.Point(514, 384);
+            this.stopBtn.Name = "stopBtn";
+            this.stopBtn.Size = new System.Drawing.Size(75, 23);
+            this.stopBtn.TabIndex = 5;
+            this.stopBtn.Text = "Завершить";
+            this.stopBtn.UseVisualStyleBackColor = true;
+            this.stopBtn.Click += new System.EventHandler(this.stopBtn_Click);
+            // 
+            // QTBox
+            // 
+            this.QTBox.Location = new System.Drawing.Point(25, 30);
+            this.QTBox.Name = "QTBox";
+            this.QTBox.Size = new System.Drawing.Size(622, 20);
+            this.QTBox.TabIndex = 6;
+            // 
+            // Answ1rB
+            // 
+            this.Answ1rB.AutoSize = true;
+            this.Answ1rB.Location = new System.Drawing.Point(74, 155);
+            this.Answ1rB.Name = "Answ1rB";
+            this.Answ1rB.Size = new System.Drawing.Size(85, 17);
+            this.Answ1rB.TabIndex = 7;
+            this.Answ1rB.TabStop = true;
+            this.Answ1rB.Text = "radioButton1";
+            this.Answ1rB.UseVisualStyleBackColor = true;
+            this.Answ1rB.CheckedChanged += new System.EventHandler(this.Answ1rB_CheckedChanged);
+            // 
+            // Answ2rB
+            // 
+            this.Answ2rB.AutoSize = true;
+            this.Answ2rB.Location = new System.Drawing.Point(74, 192);
+            this.Answ2rB.Name = "Answ2rB";
+            this.Answ2rB.Size = new System.Drawing.Size(85, 17);
+            this.Answ2rB.TabIndex = 8;
+            this.Answ2rB.TabStop = true;
+            this.Answ2rB.Text = "radioButton2";
+            this.Answ2rB.UseVisualStyleBackColor = true;
+            this.Answ2rB.CheckedChanged += new System.EventHandler(this.Answ2rB_CheckedChanged);
+            // 
+            // Answ3rB
+            // 
+            this.Answ3rB.AutoSize = true;
+            this.Answ3rB.Location = new System.Drawing.Point(74, 228);
+            this.Answ3rB.Name = "Answ3rB";
+            this.Answ3rB.Size = new System.Drawing.Size(85, 17);
+            this.Answ3rB.TabIndex = 9;
+            this.Answ3rB.TabStop = true;
+            this.Answ3rB.Text = "radioButton3";
+            this.Answ3rB.UseVisualStyleBackColor = true;
+            this.Answ3rB.CheckedChanged += new System.EventHandler(this.Answ3rB_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(208, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(205, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Введите имя и фамилию тестируемого";
+            // 
+            // NameTb
+            // 
+            this.NameTb.Location = new System.Drawing.Point(211, 100);
+            this.NameTb.Name = "NameTb";
+            this.NameTb.Size = new System.Drawing.Size(202, 20);
+            this.NameTb.TabIndex = 11;
+            // 
+            // OkBtn
+            // 
+            this.OkBtn.Location = new System.Drawing.Point(452, 100);
+            this.OkBtn.Name = "OkBtn";
+            this.OkBtn.Size = new System.Drawing.Size(34, 23);
+            this.OkBtn.TabIndex = 12;
+            this.OkBtn.Text = "ОК";
+            this.OkBtn.UseVisualStyleBackColor = true;
+            this.OkBtn.Click += new System.EventHandler(this.OkBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(675, 450);
+            this.Controls.Add(this.OkBtn);
+            this.Controls.Add(this.NameTb);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Answ3rB);
+            this.Controls.Add(this.Answ2rB);
+            this.Controls.Add(this.Answ1rB);
+            this.Controls.Add(this.QTBox);
+            this.Controls.Add(this.stopBtn);
+            this.Controls.Add(this.nextBtn);
+            this.Controls.Add(this.previousBtn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Тест";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button previousBtn;
+        private System.Windows.Forms.Button nextBtn;
+        private System.Windows.Forms.Button stopBtn;
+        private System.Windows.Forms.TextBox QTBox;
+        private System.Windows.Forms.RadioButton Answ1rB;
+        private System.Windows.Forms.RadioButton Answ2rB;
+        private System.Windows.Forms.RadioButton Answ3rB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox NameTb;
+        private System.Windows.Forms.Button OkBtn;
+    }
+}
+
---
+++ test/test/Form1.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test
+{
+    public partial class Form1 : Form
+    {
+        class Question
+        {
+           public string Text; //Текст вопроса
+           public List<string> Answer;//Лист для вариантов ответов
+           public int RightAnswer;//Правильный ответ           
+           public int CurAnswer;//Ответ, который выбрал пользователь 
+
+            //Проверка правильности ответов
+            public int Check()
+            {
+                if (CurAnswer == RightAnswer)
+                    return 1;
+                else return 0;
+            }
+
+            //Метод для отображения вопросов и вариантов ответов на форме
+             public void Show(TextBox x, RadioButton a, RadioButton b, RadioButton c)
+            {
+                x.Text = Text;
+                a.Text = Answer[0]; 
+                b.Text = Answer[1]; 
+                c.Text = Answer[2]; 
+
+                switch (CurAnswer)
+                {
+                    case 0: a.Checked = true; break;
+                    case 1: b.Checked = true; break;
+                    case 2: c.Checked = true; break;
+                }
+            }
+
+            //Метод для перемешивания вариантов ответов в вопросе
+            public void Shuffle()
+            {
+                Random rnd = new Random();
+                for (int i = 0; i < 5; i++)
+                {
+                    int x = rnd.Next(3);
+                    int y = rnd.Next(3);
+                    string t = Answer[x]; Answer[x] = Answer[y]; Answer[y] = t;
+                    //Cмещаем правильный ответ
+                    if (x == RightAnswer)
+                        RightAnswer = y;
+                    else if (y == RightAnswer)
+                        RightAnswer = x;
+                }
+            }
+
+            //Конструктор вопроса
+            public Question()
+            {
+                Answer = new List<string>(); //Создание листа ответов
+                CurAnswer = -1;//По умолчанию текущий ответ равен -1
+            }
+
+        }
+
+        List<Question> QS;//Лист из вопросов
+        int CurQ;//Храним номер текущего вопроса
+        string User; //Переменная для хранения имени пользователя
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {  
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            QS = new List<Question>();//Cоздаем новый список вопросов
+
+
+            /*Организуем доступ к файлу с вопросами.
+             Структура файла: 1-ая строка - количество вопросов;
+                              2-ая строка - вопрос;
+                              3, 4, 5 строки - варианты ответов;
+                              6-ая строка - номер правильного варианта ответов
+             */
+            System.IO.StreamReader sr = new System.IO.StreamReader("input.txt", Encoding.Default);//Создаем поток чтения и напривляем его на файл input.txt
+            //Читаем из открытого файла input.txt 1-ую строку как число
+            
+            int n = Convert.ToInt32(sr.ReadLine());
+            for (int i = 0; i < n; i++)//n раз 
+            {
+                Question Q1 = new Question();
+                Q1.Text = sr.ReadLine();//Формируем текст вопроса
+
+                //Добавляем в лист варианты ответов
+                Q1.Answer.Add(sr.ReadLine());
+                Q1.Answer.Add(sr.ReadLine());
+                Q1.Answer.Add(sr.ReadLine());
+
+                //Считываем правильный ответ как число
+                Q1.RightAnswer = Convert.ToInt32(sr.ReadLine());
+
+                //Устанавливаем текущий ответ на -1
+                Q1.CurAnswer = -1;
+
+                //Перемешиваем варианты ответов
+               Q1.Shuffle();
+                
+                //Тот вопрос, который считали, добавляем в список вопросов QS
+                QS.Add(Q1);
+            }
+            sr.Close();//Закрываем файл с вопросами после прочтения
+
+            //Если в списке только один вопрос, то кнопка "Следующий вопрос" не активна
+            if (QS.Count() == 1)
+                nextBtn.Enabled = false;
+
+            //Перемешиваем вопросы
+            Random rnd = new Random();//Заводим объект Random
+           for (int i = 0; i < QS.Count*5; i++)
+            {//Выбираем два случайных числа в диапазоне QS.Count
+                int x = rnd.Next(QS.Count);
+                int y = rnd.Next(QS.Count);
+
+            //Перемешиваем вопросы
+                Question t = QS[x]; QS[x] = QS[y]; QS[y] = t;
+            }
+           
+            
+            previousBtn.Enabled = false;//При загрузке формы кнопка "Предыдущий вопрос" не активна
+
+            CurQ = 0;//Указываем номер текущего вопроса. При загрузке он равен 0
+
+            //Отображение вопроса с вариантами ответов
+            QS[CurQ].Show(QTBox, Answ1rB, Answ2rB, Answ3rB);
+
+            //Делаем невидимыми элементы формы, кроме компонентов регистрации
+            QTBox.Visible = false;
+            Answ1rB.Visible = false;
+            Answ2rB.Visible = false;
+            Answ3rB.Visible = false;
+            previousBtn.Visible = false;
+            nextBtn.Visible = false;
+            stopBtn.Visible = false;
+        }
+
+        private void previousBtn_Click(object sender, EventArgs e)
+        {    
+            
+             CurQ--;//Уменьшаем номер текущего вопроса на 1
+             QS[CurQ].Show(QTBox, Answ1rB, Answ2rB, Answ3rB);//Показываем новый вопрос
+             
+            if (CurQ == 0) //Если номер текущего вопроса равен 0
+               previousBtn.Enabled = false;//Кнопку "Предыдущий вопрос" делаем не активной
+            
+            nextBtn.Enabled = true;//Кнопку "Следующий вопрос" делаем активной
+
+        }
+
+        private void stopBtn_Click(object sender, EventArgs e)
+        {
+            int result = 0;
+            for (int i = 0; i < QS.Count; i++)
+                result += QS[i].Check();
+            //Записываем результаты в лог-файл
+            System.IO.StreamWriter sw = new System.IO.StreamWriter("output.txt", true, Encoding.Default);
+            sw.WriteLine(User);
+            sw.WriteLine("You answer right " + result + " for " + QS.Count + " questions.");
+            sw.WriteLine();
+            sw.Close();
+            MessageBox.Show("You answer right " + result + " for " + QS.Count + " questions.");
+
+            Close();
+        }
+
+        private void nextBtn_Click(object sender, EventArgs e)
+        {
+            CurQ++;//Увеличиваем номер текущего вопроса на 1
+            QS[CurQ].Show(QTBox, Answ1rB, Answ2rB, Answ3rB);//Показываем текущий вопрос
+            
+            if (CurQ == QS.Count - 1)//Если вопрос последний в списке
+                nextBtn.Enabled = false;//Кнопку "Следующий вопрос" делаем не активной
+            
+            previousBtn.Enabled = true;//Кнопку "Предыдущий вопрос" делаем активной
+        }
+
+        private void Answ1rB_CheckedChanged(object sender, EventArgs e)
+        {
+            //Если пользователь выбрал первый чекбокс, меняем его св-во CurAnswer
+            QS[CurQ].CurAnswer = 0;
+            if (QS[CurQ].CurAnswer == QS[CurQ].RightAnswer)
+                BackColor = Color.Green;
+            else
+                BackColor = Color.LightGray;
+        }
+
+        private void Answ2rB_CheckedChanged(object sender, EventArgs e)
+        {
+            QS[CurQ].CurAnswer = 1;
+            if (QS[CurQ].CurAnswer == QS[CurQ].RightAnswer)
+                BackColor = Color.Green;
+            else
+                BackColor = Color.LightGray;
+        }
+
+        private void Answ3rB_CheckedChanged(object sender, EventArgs e)
+        {
+            QS[CurQ].CurAnswer = 2;
+            if (QS[CurQ].CurAnswer == QS[CurQ].RightAnswer)
+                BackColor = Color.Green;
+            else
+                BackColor = Color.LightGray;
+        }
+
+        private void OkBtn_Click(object sender, EventArgs e)
+        {
+            if (NameTb.Text != "")
+            {
+                User = NameTb.Text;
+                QTBox.Visible = true;
+                Answ1rB.Visible = true;
+                Answ2rB.Visible = true;
+                Answ3rB.Visible = true;
+                previousBtn.Visible = true;
+                nextBtn.Visible = true;
+                stopBtn.Visible = true;
+
+                label1.Visible = false;
+                NameTb.Visible = false;
+                OkBtn.Visible = false;
+            }
+
+
+        }
+    }
+}
